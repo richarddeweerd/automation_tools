1541c201344e9019734e3cabe7ee5c26a3729882
Richard de Weerd <richard@deweerd.biz>
2020-02-19 00:47:32 +0100

    added dbg info

--- update_airly.py
+++ update_airly.py
@@ -14,6 +14,7 @@ def main():
     air = airly.Airly(config["Airly"]["Host"], config["Airly"]["APIKey"], config["Airly"]["Installation"])
     if air.update() == 200:
         if air.temp:
+            print(air.temp)
             dom.set_sensor_thb(config["idx"]["airly_thb"], air.temp, air.hum, air.baro)
 
         if air.pm1:
